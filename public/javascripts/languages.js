07dec6c4b46fe2e94085dca87c486b6214c67dac
Aliou S Maiga <badrasadja@gmail.com>
2017-06-20 17:26:49 -0700

    Another language support update. Now all placeholders get changed when changing language

--- public/javascripts/languages.js
+++ public/javascripts/languages.js
@@ -3,19 +3,23 @@ $(function() {
   //Read the cookie, if it has been previously set
   var language = Cookies('language')
   //Set language to previously set value
-    !language || $('#sel').val( language );
+  !language || $('#sel').val( language );
 
-    //Set up an event listener to update the cookie whenever language is changed
- $('select').change(function(event){
+  //Set up an event listener to update the cookie whenever language is changed
+  $('select').change(function(event){
+    Cookies.remove('language', language)
 
-   Cookies.remove('language', language)
+    var language = this.value;
+    $('.lang, .linkdeleteuser').each(function(index, element){
+      //  changing language
+      $(this).text(arrLang[language][$(this).attr('key')]);
+      // changing placeholder language
+      var placeholderValues = $(this).text();
+      $(this).attr('placeholder', placeholderValues);
 
-   var language = this.value;
-   $('.lang').each(function(index, element){
-    $(this).text(arrLang[language][$(this).attr('key')]);
-    Cookies.set('language', language)
-   })
+      Cookies.set('language', language);
+    })
 
- })
- .change();
+  })
+  .change();
 });
